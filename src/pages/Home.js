9c48f110850c5daa578486255437e9022def99bb
Tushar-Gautam <tushargautam999@gmail.com>
2022-12-01 15:33:03 +0530

    navbar made responsive and sticky prop to navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import LandingPageComponent from "../components/HomePageComponents/LandingPage";
 
 function Home() {
   return (
     <div style={{ minHeight: "100vh" }}>
       <Header />
+      <LandingPageComponent />
       <Footer />
     </div>
   );
